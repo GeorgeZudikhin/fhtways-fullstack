cabb045ca7f1e5f438b49bcf3d52afbc9b7a0574
MinMin <minesungur@outlook.de>
2023-12-19 00:10:47 +0100

    button,footer,nextPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import logo from './assets/Logo.jpg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom'; 
+import Logo from './assets/logo.jpg';
 import fhtwlogo from  './assets/fhtw_logo.svg.png';
 import adiobook from  './assets/audiobook.PNG';
 import aplus from  './assets/aplus.PNG';
@@ -9,45 +11,91 @@ import znormal from  './assets/znormal.PNG';
 import zplus from  './assets/zplus.PNG';
 import returnz from  './assets/return.PNG';
 import sprache from  './assets/sprache.PNG';
+import Moodle from  './assets/icons/moodle.png';
+import Cis from  './assets/icons/cis.png';
+import Linkedin from  './assets/icons/linkedin.png';
+import Email from  './assets/icons/mail.png';
 
 import './App.css';
+import App2 from './App2.js';
 
-function App() {
+
+function MainApp() {
+  const [contrastMode, setContrastMode] = useState(false);
+  const [fontSize, setFontSize] = useState(16); // Initial font size of 16px
+  const [isParagraphLarge, setIsParagraphLarge] = useState(false);
+  const [lineHeight, setLineHeight] = useState(1.5); // Initial line height
+  const [reset] = useState(1.5); // Initial line height
+  const toggleContrast = () => {
+    setContrastMode(!contrastMode);
+    // Logic to change contrast mode and colors accordingly
+    if (!contrastMode) {
+      document.body.style.backgroundColor = '#000000';
+      document.body.style.color = '#ffffff';
+    } else {
+      document.body.style.backgroundColor = '#ffffff';
+      document.body.style.color = '#000000';
+    }
+  };
+
+  const resetContrast = () => {
+    setContrastMode(false);
+    document.body.style.backgroundColor = '#ffffff';
+    document.body.style.color = '#000000';
+  };
+
+  const increaseFontSize = () => {
+    setFontSize(fontSize + 4); // Schriftgröße um 4 Pixel erhöhen
+    setIsParagraphLarge(true); // Setze den Zustand für den vergrößerten Absatz auf true
+  };
+
+  const resetFontSize = () => {
+    setFontSize(16); // Schriftgröße auf 16 Pixel zurücksetzen
+    setIsParagraphLarge(false); // Setze den Zustand für den vergrößerten Absatz auf false
+  };
+  const increaseLineHeight = () => {
+    setLineHeight(lineHeight + 0.2); // Zeilenabstand um 0.2 erhöhen
+  };
+
+  const resetLineHeight = () => {
+    setLineHeight(1.5); // Zeilenabstand zurücksetzen
+  };
+  const resetAll = () => {
+    setFontSize(16);
+    setLineHeight(1.5);
+    setContrastMode(false);
+    document.body.style.backgroundColor = '#ffffff';
+    document.body.style.color = '#000000';
+  };
     return (
-        <div className="App">
-             <div className="top-right-buttons">
+        <div className={`App ${contrastMode ? 'contrast-mode' : ''}`} style={{ fontSize: `${fontSize}px`, lineHeight: lineHeight }}>                <div className="top-right-buttons">             
                 <a href="#" onClick={() => { /* Action for Button 1 */ }}>
                     <img className="top-image-button" src={adiobook} alt="Button 1" />
                 </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={aplus} alt="Button 1" />
-                </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={aminus} alt="Button 1" />
-                </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={bnw} alt="Button 1" />
-                </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={farbe} alt="Button 1" />
-                </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={znormal} alt="Button 1" />
-                </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={zplus} alt="Button 1" />
-                </a>
-                <a href="#" onClick={() => { /* Action for Button 1 */ }}>
-                    <img className="top-image-button" src={returnz} alt="Button 1" />
-                </a>
+                
+                <a href="#" onClick={increaseFontSize}><img className="top-image-button" src={aplus} alt="Button 2" /></a>
+                <a href="#" onClick={resetFontSize}><img className="top-image-button" src={aminus} alt="Button 3" /></a>
+
+                <a href="#" onClick={toggleContrast}><img className="top-image-button" src={bnw} alt="Button 4" /></a>
+                <a href="#" onClick={resetContrast}><img className="top-image-button" src={farbe} alt="Button 5" /></a>
+               
+                <a href="#" onClick={increaseLineHeight}><img className="top-image-button" src={zplus} alt="Button 6" /></a>
+                <a href="#" onClick={resetLineHeight}><img className="top-image-button" src={znormal} alt="Button 7" /></a>
+               
+                <a href="#" onClick={resetAll}><img className="top-image-button" src={returnz} alt="Button 8" /></a>
                 <a href="#" onClick={() => { /* Action for Button 1 */ }}>
                     <img className="top-image-button" src={sprache} alt="Button 1" />
                 </a>
                 
             </div>
             <div className={'logo-container'}>
-                <img  className={'school-logo'} src={fhtwlogo}/>
-                <img className={'project-logo'} src={logo}/>
+            <img className={'project-logo'} src={Logo} alt="Project Logo" />
+
+                <a href="https://www.technikum-wien.at/" target="_blank" rel="noopener noreferrer">
+                <div className={'logo-container'}>
+                    <img className={'school-logo'} src={fhtwlogo} alt="FHTW Logo" />
+                </div>
+                </a>
             </div>
             
             
@@ -56,15 +104,17 @@ function App() {
             <div className="content-container" style={{ textAlign: 'center' }}>
                 <h1 style={{ color: '#0a65c0' }}>Pathfinding for All - Enter Your Route and Explore FHTW</h1>
                 </div>
-                <div className="content-container">             
-                    <p>Mit FHTWays können Sie durch die FHTW navigieren! Geben Sie Ihren Startpunkt und Ihr Ziel in das vorgesehene Format ein:
+                
+                <div className="content-container"style={{ fontSize: `${fontSize}px` }}>        
+                    <p style={{ fontSize: isParagraphLarge ? '24px' : 'inherit'}}>
+                        Mit FHTWays können Sie durch die FHTW navigieren! Geben Sie Ihren Startpunkt und Ihr Ziel in das vorgesehene Format ein:
                     [Gebäude][Stockwerk]'Punkt'[Raum]. Beispiel: F4.24 für Gebäude F, 4. Stockwerk, Raum 24. Klicken Sie auf den
                     "Los"-Button oder drücken Sie die Enter-Taste, um Ihre Routenanfrage zu starten und folgen Sie den detaillierten
                     Wegbeschreibungen!</p>
-
+                    
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
                     <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr auto', alignItems: 'center', columnGap: '10px', marginBottom: '5px' }}>
-                        <p style={{ fontWeight: 'bolder', margin: '0', fontSize: '16px' }}>Start</p>
+                        <p style={{ fontWeight: 'bolder', margin: '0', fontSize: '32px' }}>Start</p>
                         <div style={{ display: 'flex', alignItems: 'center', marginLeft: '-5px' }}>
                             <div className={'search-bar'} style={{ width: '100%' }}>
                                 <input
@@ -77,7 +127,7 @@ function App() {
                         <div style={{ width: '20px' }}></div>
                     </div>
                     <div style={{ display: 'grid', gridTemplateColumns: 'auto 1fr auto', alignItems: 'center', columnGap: '10px', marginBottom: '5px' }}>
-                        <p style={{ fontWeight: 'bold', margin: '0', fontSize: '16px' }}>Ziel</p>
+                        <p style={{ fontWeight: 'bold', margin: '0', fontSize: '32px'}}>Ziel</p>
                         <div style={{ display: 'flex', alignItems: 'center', marginLeft: '-5px' }}>
                             <div className={'search-bar'} style={{ width: '100%' }}>
                                 <input
@@ -92,24 +142,57 @@ function App() {
                 </div>
 
                      <div className={'button-container'}>
-                         <button>Los!</button>
+                        <Link to="/app2">
+                            <button>Los!</button>
+                        </Link>
                      </div>
-                    <p> *mit den Buchstaben "M", "W" oder “D” im [Zimmer] können Sie direkt zu den nächstliegenden Herren-, Damen-, Diverstoiletten navigieren</p>
-                    <p> *für den Eingang ins Gebäude verwenden Sie einfach die Buchstabe des jeweiligen Gebäudes, z.B. F für das Gebäude </p>
+                    <p style={{ fontSize: isParagraphLarge ? '24px' : 'inherit'}}> *mit den Buchstaben "M", "W" oder “D” im [Zimmer] können Sie direkt zu den nächstliegenden Herren-, Damen-, Diverstoiletten navigieren</p>
+                    <p style={{ fontSize: isParagraphLarge ? '24px' : 'inherit'}}> *für den Eingang ins Gebäude verwenden Sie einfach die Buchstabe des jeweiligen Gebäudes, z.B. F für das Gebäude </p>
                 </div>
             </div>
-            <nav className={'footer-nav'} style={{ textAlign: 'center' }}>
-                <ul style={{ listStyle: 'none', padding: 0 }}>
-                 <li>Startseite</li>
-                 <li>Impressum</li>
-                 
-                 <li>Kontakt</li>
-                 <li>Über uns</li>
-             </ul>
-    <p style={{ fontSize: '15px', color: 'white', fontWeight: 'bold' }}>© Copyright 2023 - FHTWays</p>
-</nav>
+            <nav className={'footer-nav'} style={{ textAlign: 'center'}}>
+                 <ul style={{ listStyle: 'none', padding: 0 }}>
+                <li>Startseite</li>
+                <li>Impressum</li>
+                <li>Kontakt</li>
+                <li>Über uns</li>
+                </ul>
+                <ul style={{ listStyle: 'none', padding: 0, display: 'flex', justifyContent: 'center' }}>
+                <li>
+                    <a href="https://moodle.technikum-wien.at/" target="_blank" rel="noopener noreferrer">
+                    <img className="top-image-button" src={Moodle} alt="Moodle" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://cis.technikum-wien.at/cis/index.html" target="_blank" rel="noopener noreferrer">
+                    <img className="top-image-button" src={Cis} alt="Cis" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/school/uas-technikum-wien/?originalSubdomain=at" target="_blank" rel="noopener noreferrer">
+                    <img className="top-image-button" src={Linkedin} alt="Linkedin" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://sogo.technikum-wien.at/SOGo/">
+                    <img className="top-image-button" src={Email} alt="Email" />
+                    </a>
+                </li>
+        </ul>
+             <p style={{ fontSize: '15px', color: 'white', fontWeight: 'bold' }}>© Copyright 2023 - FHTWays</p>
+            </nav>
         </div>
     );
 }
-export default App;
 
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainApp />} />
+                <Route path="/app2" element={<App2 />} />
+            </Routes>
+        </Router>
+    );
+}
+export default App;
