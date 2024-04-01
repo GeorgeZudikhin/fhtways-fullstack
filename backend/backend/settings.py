dcd49e324ea3a2f3293fda5c22450b635ef7e385
Patrik Karasek <patrikkarasek@Patriks-MacBook-Pro.local>
2024-04-01 20:10:20 +0200

    Set up database with node and edge data and admin interface for management

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,8 +80,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db', 
+        'USER': 'postgres',  
+        'PASSWORD': 'postgres',  
+        'HOST': 'localhost',  
+        'PORT': '5432',  
     }
 }
 
